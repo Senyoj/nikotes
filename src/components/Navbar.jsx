d1d926e9fa72aac7e44473597955c833d2b64639
Joel Senyo <116897993+Senyoj@users.noreply.github.com>
2024-04-02 23:36:02 +0000

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { logo, Bars, win_kitchen } from "../assets";
-import { FaCartPlus, FaSearch, FaBars } from "react-icons/fa";
+import { FaCartPlus, FaSearch, FaBars, FaTimes } from "react-icons/fa";
 import SearchBar from "./SearchBar";
 import Dropdown from "./Dropdown ";
 import { Link } from "react-router-dom";
@@ -34,6 +34,10 @@ const Navbar = () => {
   // toggle mobile  visibility
   const ToggleMobile = () => {
     setIsMobileOpen(!isMobileOpen);
+  };
+
+  const CloseMobileNav = () => {
+    setIsMobileOpen(false);
   };
 
   return (
@@ -104,7 +108,37 @@ const Navbar = () => {
         </div>
       </div>
 
-      {isMobileOpen && <div className="relative w-full ">asdad</div>}
+      {isMobileOpen && (
+        <div className=" w-full  text-white">
+          <div
+            className="bg-textColor bg-opacity-50 w-full h-[100vh] absolute z-20 top-0 left-0 p-5"
+            style={{ backdropFilter: "blur(10px)" }}
+          >
+            <div>
+            <button className="bg-textColor bg-opacity-20 p-1" onClick={CloseMobileNav}>
+              <FaTimes />
+            </button>
+            </div>
+            <div className="">
+            <ul className="flex flex-col justify-center items-end gap-5 text-lg font-semibold ">
+                <li className="flex justify-center items-center cursor-pointer ">
+                  <Link to="">Food App</Link>
+                  <img src={win_kitchen} alt="" />
+                </li>
+                <li>
+                  <Link to="">Accessories</Link>
+                </li>
+                <li>
+                  <Link to="">Latops</Link>
+                </li>
+                <li>
+                  <Link to="">Watch</Link>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
